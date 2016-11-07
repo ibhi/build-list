22e386294eddae23cc5d192f0136666c1ed00241
ibhi <ibrahimin2004@gmail.com>
2016-11-08 00:12:05 +0530

    Fix: Linting warnings in unit test

--- src/app/accordion-group/accordion-group.component.spec.ts
+++ src/app/accordion-group/accordion-group.component.spec.ts
@@ -2,11 +2,11 @@
 
 import { ComponentFixture, TestBed, async } from '@angular/core/testing';
 import { AccordionGroupComponent } from './accordion-group.component';
-import { AccordionComponent } from '.././accordion/accordion.component'
+import { AccordionComponent } from '.././accordion/accordion.component';
 describe('Component: AccordionGroup', () => {
   let comp: AccordionGroupComponent;
   let fixture: ComponentFixture<AccordionGroupComponent>;
-  
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [AccordionGroupComponent],
